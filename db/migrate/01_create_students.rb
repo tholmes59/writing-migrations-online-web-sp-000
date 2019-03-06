b08568d07fb5b90b7673331b78a5cebb8e457cbf
Tom Holmes <tholmes59@gmail.com>
2019-03-06 00:30:40 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,8 +1,7 @@
-class CreateStudents < ActiveRecord::Migration[5.1]
-  
+class CreateStudents < ActiveRecord::Migration
   def change
-    create_table student do |t|
+    create_table :students do |t|
       t.string :name
     end
-  end 
+  end
 end
