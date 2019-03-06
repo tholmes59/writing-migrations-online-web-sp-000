db1a6e34f1e48ecff65ebd27e778ead30072e099
Tom Holmes <tholmes59@gmail.com>
2019-03-06 00:32:18 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -5,9 +5,3 @@ class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[4.2]
   end
 end
 
-# class AddGradeAndBirthdateToStudents < ActiveRecord::Migration
-#   def change
-#     add_column :students, :grade, :integer
-#     add_column :students, :birthdate, :string
-#   end
-# end
